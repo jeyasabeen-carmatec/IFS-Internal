88c613b636c89377f9193bb82d5881995759eb9a
anumolu mac mini <anumolumacmini@anumolus-Mac-mini.local>
2018-06-04 12:49:56 +0530

    lcl code

--- QIFS/Lang_Sources/Defines.h
+++
@@ -1,9 +0,0 @@
-//
-//  Defines.h
-//  ios_language_manager
-//
-//  Created by Maxim Bilan on 1/10/15.
-//  Copyright (c) 2015 Maxim Bilan. All rights reserved.
-//
-
-#define USE_ON_FLY_LOCALIZATION
